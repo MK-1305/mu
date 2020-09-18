6f1f12cbb23f81541a06890d4e9276804a6f204b
vagrant <vagrant@localhost.localdomain>
2020-09-18 14:11:52 +0000

    image

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -19,9 +19,9 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # def default_url
-  #     "/images/" + [version_name, "no_image.png"].compact.join('_')
-  # end
+  def default_url
+      "/images/" + [version_name, "no_image.png"].compact.join('_')
+  end
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   # #   # For Rails 3.1+ asset pipeline compatibility:
