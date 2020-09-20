a3685daf1b41a69f680ccad51e93e67f515c4521
vagrant <vagrant@localhost.localdomain>
2020-09-20 02:36:10 +0000

    [change]profile_image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_29_051040) do
+ActiveRecord::Schema.define(version: 2020_09_20_023204) do
 
   create_table "accepts", force: :cascade do |t|
     t.integer "user_id"
@@ -133,7 +133,7 @@ ActiveRecord::Schema.define(version: 2020_08_29_051040) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name"
-    t.integer "profile_image"
+    t.string "profile_image"
     t.boolean "is_unsubscribe", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
