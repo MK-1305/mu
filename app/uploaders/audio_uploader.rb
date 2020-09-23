adb6d9936eb7fa15d4e3538d2ff7afe205b6ef56
vagrant <vagrant@localhost.localdomain>
2020-09-23 05:12:02 +0000

    [add]file

--- app/uploaders/audio_uploader.rb
+++ app/uploaders/audio_uploader.rb
@@ -5,7 +5,11 @@ class AudioUploader < CarrierWave::Uploader::Base
   include CarrierWave::Audio
 
   # Choose what kind of storage to use for this uploader:
-  storage :fog
+  if Rails.env.production?
+    storage :fog # 本番環境のみ
+  else
+    storage :file # 本番環境以外
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
