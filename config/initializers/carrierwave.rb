4d2764d840902a9f5133263594e679a71a9aa5e5
vagrant <vagrant@localhost.localdomain>
2020-09-23 04:32:11 +0000

    success_upload

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,20 +1,20 @@
-#   require 'carrierwave/storage/abstract'
-#   require 'carrierwave/storage/file'
-#   require 'carrierwave/storage/fog'
+  require 'carrierwave/storage/abstract'
+  require 'carrierwave/storage/file'
+  require 'carrierwave/storage/fog'
 
-# CarrierWave.configure do |config|
+CarrierWave.configure do |config|
 
-#   config.storage :fog
-#   config.fog_provider = 'fog/aws'
-#   config.fog_directory  = 'mu-file-s3'
-#   config.asset_host = 'https://mu-file-s3.s3.amazonaws.com'
-#   # config.asset_host = 'https://s3.amazonaws.com/mu-file-s3'
-#   # config.fog_public = false
-#   config.fog_credentials = {
-#     provider: 'AWS',
-#     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-#     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-#     region: 'ap-northeast-1',
-#     path_style: true
-#   }
-# end
+  config.storage :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_directory  = 'mu-file-s3'
+  config.asset_host = 'https://mu-file-s3.s3.amazonaws.com'
+  # config.asset_host = 'https://s3.amazonaws.com/mu-file-s3'
+  # config.fog_public = false
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region: 'ap-northeast-1',
+    path_style: true
+  }
+end
